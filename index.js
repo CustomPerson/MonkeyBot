028259b3007b074c48056a9f96927ad5e34c2a97
CustomPerson <anzxesdev@gmail.com>
2021-08-05 15:18:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const Discord = require('discord.js')
-
-const client = new Discord.Client();
+const client = new Client();
 
 client.once('ready', () => {
     console.log('MonkeyBot is online!');
 })
 
+client.once("message", (message) => {
+    if(message.author.bot) return;
+    if(message.channel.type !== 'text') return;
+    let prefix = '|';
+    let MessageArray = message.content.split('');
+    let cmd = MessageArray[0].slice(prefix.length)
+    let args = MessageArray.slice(1)
 
+    if(|message.content.startsWith(prefix)) return;
+
+    if(cmd == 'help') {
+    let member = message.mentions.members.first();
+    message.channel.send('No lol')
+    }
+
+    if(cmd == 'Help') {
+    let member = message.mentions.members.first();
+    if(message.channel.send('No lol')) else {
+        message.channel.send('No lol ${}')
+     }
+    }
+
+})
 
 
 
